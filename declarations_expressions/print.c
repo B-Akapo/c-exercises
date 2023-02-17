891ced9e0203f452f6011250d597e96c302aab61
B-Akapo <bunmi.i.akapo@gmail.com>
2023-02-17 14:27:25 +0100

    create print.c

---
+++ declarations_expressions/print.c
@@ -0,0 +1,17 @@
+/*
+Author: Oluwabunmi Akapo
+Date: 17th February 2023
+Description: program to print your name, date of birth. and mobile number.
+*/
+
+#include <stdio.h> 
+
+int main()  
+{
+    printf("Name   : Alexandra Abramov\n"); 
+    printf("DOB    : July 14, 1975\n"); 
+    printf("Mobile : 99-9999999999\n"); 
+    return(0); 
+}
+
+
