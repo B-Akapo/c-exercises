c77a43fe6d4027aa63e20598e11f942984e59895
B-Akapo <bunmi.i.akapo@gmail.com>
2023-02-18 16:00:29 +0100

    update 3-days.c

--- declarations_expressions/3-days.c
+++ declarations_expressions/3-days.c
@@ -19,8 +19,13 @@ int main(void)
    printf("Enter number of days: ");
    scanf("%d", &num_of_days);
 
+   // calculate number of years
    years = num_of_days / DAYS_IN_A_YEAR;
+
+   // calculate number of weeks
    weeks = (num_of_days % DAYS_IN_A_YEAR) / DAYS_IN_A_WEEK;
+
+   // calculate number of days
    days = (num_of_days % DAYS_IN_A_YEAR) % DAYS_IN_A_WEEK;
 
    printf("In %d there are %d years, %d weeks and %d days\n", num_of_days, years, weeks, days);
