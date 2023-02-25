4168589e7eee1913bf286dc6d56d93b8f3e4a063
B-Akapo <bunmi.i.akapo@gmail.com>
2023-02-25 14:00:23 +0100

    updated 11-breaks.c

--- declarations_expressions/11-breaks.c
+++ declarations_expressions/11-breaks.c
@@ -47,4 +47,36 @@ int main(void)
     one = ((((((num % 100) % 50) % 20) % 10) % 5) % 2) / 1;
     total = one;
     printf("%d Note(s) of 1.00\n", total);
+
+    return 0;
+
+    /*
+    int amount;
+    printf("Input the amount: ");
+    scanf("%d", &amount);
+
+    int note100 = amount / 100;
+    amount %= 100;
+    int note50 = amount / 50;
+    amount %= 50;
+    int note20 = amount / 20;
+    amount %= 20;
+    int note10 = amount / 10;
+    amount %= 10;
+    int note5 = amount / 5;
+    amount %= 5;
+    int note2 = amount / 2;
+    amount %= 2;
+    int note1 = amount;
+
+    printf("There are:\n");
+    printf("%d Note(s) of 100.00\n", note100);
+    printf("%d Note(s) of 50.00\n", note50);
+    printf("%d Note(s) of 20.00\n", note20);
+    printf("%d Note(s) of 10.00\n", note10);
+    printf("%d Note(s) of 5.00\n", note5);
+    printf("%d Note(s) of 2.00\n", note2);
+    printf("%d Note(s) of 1.00\n", note1);
+    */
 }
+
